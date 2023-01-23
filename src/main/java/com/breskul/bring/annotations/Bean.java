8b9456097ad607f0476a98c785f2ba92c24e9674
Gleb Nogai <Gleb.Nogai@nordstrom.com>
2023-01-23 15:44:57 +0200

    Added basic annotations for general Bring creation.

---
+++ src/main/java/com/breskul/bring/annotations/Bean.java
@@ -0,0 +1,24 @@
+package com.breskul.bring.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Indicates that a method produces a bean which will be stored and managed in Bring container.
+ *
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Bean {
+
+  /**
+   * The name of this bean. If left unspecified
+   * the name of the bean is the name from annotated method.
+   */
+  String value() default "";
+
+}
---
+++ src/main/java/com/breskul/bring/annotations/Configuration.java
@@ -0,0 +1,24 @@
+package com.breskul.bring.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that a class declares one or more {@link Bean @Bean} methods and
+ * using by Bring for generating and getting bean instances.
+ *
+ */
+
+@Component
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Configuration {
+
+  /**
+   * Specific bean name for Configuration class.
+   */
+  String value() default "";
+
+}
