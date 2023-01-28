c972badb6ecb5d47f8d42c515a9d11470e2294fd
yuriyfomin17 <fominyura2012@gmail.com>
2023-01-28 17:54:34 +0000

    - cleaning up

--- src/main/java/com/breskul/bring/demoComponents/ApplicationContextDemo.java
+++ src/main/java/com/breskul/bring/demoComponents/ApplicationContextDemo.java
@@ -3,16 +3,11 @@ package com.breskul.bring.demoComponents;
 import com.breskul.bring.AnnotationConfigApplicationContext;
 import com.breskul.bring.ApplicationContext;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Parameter;
-import java.util.HashMap;
-import java.util.Map;
-
 public class ApplicationContextDemo {
     private static final String message = "HELLO_BRESKUL_APPLICATION_CONTEXT";
     private static final String PACAKGE_NAME = "com.breskul.bring.demoComponents";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(PACAKGE_NAME);
