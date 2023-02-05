9a9fcdc3bceae16b8593599ac0db8efb6eb4cbc9
Artem Yankovets <artem.yankovets@sdk.finance>
2023-02-06 00:42:11 +0200

    [feature] added common exception class

--- src/main/java/com/breskul/bring/exceptions/NoUniqueBeanException.java
+++ src/main/java/com/breskul/bring/exceptions/NoUniqueBeanException.java
@@ -1,10 +1,16 @@
 package com.breskul.bring.exceptions;
 
 /**
- * No unique Bean exception
+ * Throws when no unique Bean found
+ *
+ * @author Artem Yankovets
  */
-public class NoUniqueBeanException extends RuntimeException{
-    public NoUniqueBeanException(String message) {
-        super(message);
-    }
+public class NoUniqueBeanException extends CommonException {
+
+  public static final String CAUSE_BEAN_NAME_PATTERN = "The bean (name: %s) is not unique";
+  public static final String SUGGESTED_SOLUTION = "Check the Bean name. It should be unique";
+
+  public NoUniqueBeanException(String location, String beanName) {
+    super(location, String.format(CAUSE_BEAN_NAME_PATTERN, beanName), SUGGESTED_SOLUTION);
+  }
 }
